ae48dddc4b86883b0fce28f117ac318921a83a5a
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-16 21:57:39 +0900

    00005

---
+++ cp6-5/cp6-5/TopMainView.swift
@@ -0,0 +1,60 @@
+//
+//  TopMainView.swift
+//  cp6-5
+//
+//  Created by 酒井典昭 on 2019/09/16.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+protocol TopMainViewDelegate: NSObjectProtocol{
+    
+}
+
+extension TopMainViewDelegate {
+    
+}
+// MARK: - Property
+class TopMainView: BaseView {
+    weak var delegate: TopMainViewDelegate? = nil
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    var labels: [String] = ["あああああああああああああああああああああああ","いいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい","うううううううううううううううううううううううううううううううううううううううううううう","うううううううううううううううううううううううううううううううううううううううううううう","えええええええええええええええええええええええええええええええ","おおおおおおおおおおおおおおおおおおおおおおおおおおおおおお","えええええええええええええええええええええええええええええええええええええええ","あああああああああああああああああああああああ","いいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい","うううううううううううううううううううううううううううううううううううううううううううう","うううううううううううううううううううううううううううううううううううううううううううう","えええええええええええええええええええええええええええええええ","おおおおおおおおおおおおおおおおおおおおおおおおおおおおおお","えええええええええええええええええええええええええええええええええええええええ"]
+}
+
+// MARK: - Life cycle
+extension TopMainView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        collectionView.dataSource = self
+        loadCollectionViewCellFromXib(collectionView: collectionView, cellName: "TopMainCollectionViewCell")
+    }
+    override func draw(_ rect: CGRect) {
+        super .draw(rect)
+        collectionViewFlowLayout.estimatedItemSize = CGSize(width: collectionView.contentSize.width, height: 10)
+    }
+}
+
+// MARK: - Protocol
+extension TopMainView: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return labels .count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: TopMainCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TopMainCollectionViewCell", for: indexPath)as? TopMainCollectionViewCell else {return UICollectionViewCell() }
+        cell.label.text = labels[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+// MARK: - method
+extension TopMainView {
+    
+}
+
